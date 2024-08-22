ed9f854ecf0c0bd37a25906ba1caeba34da8f675
shaunpereira13 <shaunpereira03@gmail.com>
2024-08-22 22:14:35 +0530

    made the logo a button

--- src/components/Pointer/Pointer.jsx
+++ src/components/Pointer/Pointer.jsx
@@ -5,15 +5,13 @@ import Vector3 from "../Vector 3/Vector3";
 
 const Pointer = (props) => {
   return (
-    <>
-    <div className="pointer">
-      <img src={symbols.flame} alt="" />
-      <h2>{props.title}</h2>
-      
+    <div>
+      <div className="pointer">
+        <img src={symbols.flame} alt="" />
+        <h2>{props.title}</h2>
+      </div>
+      <Vector3></Vector3>
     </div>
-    <Vector3></Vector3>
-    </>
-   
   );
 };
 export default Pointer;
